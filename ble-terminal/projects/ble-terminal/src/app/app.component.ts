047d5501d8832ab53cac814dc04a84e4e290bcf2
Mathias Köppchen - Protronic GmbH <m.koeppchen@protronic-gmbh.de>
2023-03-14 12:32:40 +0100

    add ng-terminal basic example

--- ble-terminal/projects/ble-terminal/src/app/app.component.ts
+++ ble-terminal/projects/ble-terminal/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { FunctionsUsingCSI, NgTerminal } from 'ng-terminal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ble-terminal';
+
+export class AppComponent implements AfterViewInit {
+  readonly title = 'ble-terminal';
+  readonly prompt = '\n' + FunctionsUsingCSI.cursorColumn(1) + '$ ';
+  @ViewChild('term', { static: false }) child!: NgTerminal;
+
+  ngAfterViewInit(): void {
+    this.child.onData().subscribe((input) => {
+      if (input === '\r') { // Carriage Return (When Enter is pressed)
+        this.child.write(this.prompt);
+      } else if (input === '\u007f') { // Delete (When Backspace is pressed)
+        if (this.child.underlying.buffer.active.cursorX > 2) {
+          this.child.write('\b \b');
+        }
+      } else if (input === '\u0003') { // End of Text (When Ctrl and C are pressed)
+        this.child.write('^C');
+        this.child.write(this.prompt);
+      } else
+        this.child.write(input);
+    });
+  }
 }
